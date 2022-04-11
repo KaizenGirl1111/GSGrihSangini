8a629453f6a5977f2e72f026933eeeed032b19f4
Ayush <ayushthth@gmail.com>
2022-04-11 20:10:14 +0530

    BackendEndpoints

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,10 @@ const port=process.env.PORT || 5000
 app.use(express.json())
  
 
+const userRoute=require('./routes/user')
+app.use(userRoute)
+
+
 app.listen(port,()=>{
     console.log("Backend server is listening on port 5000")
 })
