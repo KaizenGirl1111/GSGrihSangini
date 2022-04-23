d4e5ea64602c1586928666719f57fae8c8e4a8f4
Siddhesh Jain <jainsiddhesh777@gmail.com>
2022-04-23 15:54:52 +0530

    confirm password field added

--- src/Components/App/Login.jsx
+++ src/Components/App/Login.jsx
@@ -1,50 +1,69 @@
-import React from 'react';
-import "./login.css"
+import React from "react";
+import "./login.css";
 import {
   MDBInput,
   MDBCol,
   MDBRow,
   MDBCheckbox,
   MDBBtn,
-  MDBIcon
-} from 'mdb-react-ui-kit';
+  MDBIcon,
+} from "mdb-react-ui-kit";
 
 function Signup() {
   return (
-    <form style={{
-      width: "22rem",
-      alignItems: "center",
-      justifyContent: "center",
-      margin: "auto",
-      paddingTop: '100px',
-
-
-    }}>
+    <form
+      style={{
+        width: "22rem",
+        alignItems: "center",
+        justifyContent: "center",
+        margin: "auto",
+        paddingTop: "100px",
+      }}
+    >
       <div class="card">
-        <div class="block"><h1 class="heading text-center mb-4"><b>Login</b></h1></div>
+        <div class="block">
+          <h1 class="heading text-center mb-4">
+            <b>Login</b>
+          </h1>
+        </div>
 
-        <MDBInput className='mb-4' type='email' id='form2Example1' label='Email' />
-        <MDBInput className='mb-4' type='password' id='form2Example2' label='Password' />
+        <MDBInput
+          className="mb-4"
+          type="email"
+          id="form2Example1"
+          label="Email"
+        />
+        <MDBInput
+          className="mb-4"
+          type="password"
+          id="form2Example2"
+          label="Password"
+        />
+        <MDBInput
+          className="mb-4"
+          type="password"
+          id="form2Example2"
+          label="Confirm Password"
+        />
         <div class="rem">
-          <MDBRow className='mb-4'>
-            <MDBCol className='d-flex justify-content-center'>
-              <MDBCheckbox id='form2Example3' label='Remember me' defaultChecked />
+          <MDBRow className="mb-4">
+            <MDBCol className="d-flex justify-content-center">
+              <MDBCheckbox
+                id="form2Example3"
+                label="Remember me"
+                defaultChecked
+              />
             </MDBCol>
-
           </MDBRow>
         </div>
-        <MDBBtn type='submit' className='mb-4' block color='white'>
+        <MDBBtn type="submit" className="mb-4" block color="white">
           Login
         </MDBBtn>
-
-
       </div>
 
-      <div className='text-center'>
-
-        
+      <div className="text-center">
         <div className="row my-4 d-flex justify-content-center">
-          <div >
+          <div>
             <MDBBtn
               type="button"
               color="white"
@@ -60,16 +79,10 @@ function Signup() {
               rounded
               className="mr-md-4 z-depth-1a"
             >
-
               <MDBIcon fab icon="google-plus-g" className="blue-text" />
             </MDBBtn>
           </div>
-
         </div>
-
-
-
-
       </div>
     </form>
   );
