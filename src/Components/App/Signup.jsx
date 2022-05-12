864c39d10456caa45bd4434aa7ae9823ae43626e
Siddhesh Jain <jainsiddhesh777@gmail.com>
2022-05-12 15:10:55 +0530

    Signup page ui

--- src/Components/App/Signup.jsx
+++ src/Components/App/Signup.jsx
@@ -1,138 +1,91 @@
 import React from "react";
 import "./signup.css";
 import { Link } from "react-router-dom";
+import { Fragment, useState } from "react";
 import NavigationBar from "./NavigationBar/NavigationBar";
 import Footer from "./Footer/Footer";
-import {
-  MDBInput,
-  MDBCol,
-  MDBRow,
-  MDBCheckbox,
-  MDBBtn,
-  MDBIcon,
-} from "mdb-react-ui-kit";
+import {RiLockPasswordFill} from "react-icons/ri"
+import {MdEmail} from "react-icons/md"
+import {BsFillTelephoneFill} from "react-icons/bs"
+
 
 function Signup() {
-  const [email, setEmail] = React.useState("");
-  const [contactNumber, setContactNumber] = React.useState();
-  const [password, setPassword] = React.useState("");
-  const [cpassword, setCpassword] = React.useState("");
-  const [address, setAddress] = React.useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cpassword, setCpassword] = useState("");
+  const [contactNo, setContactNo] = useState("");
+  const [address,setAddress] = useState("");
 
-  function handleSubmit() {
-    console.log(email);
-    console.log(password);
-    console.log(cpassword);
-    console.log(contactNumber);
-    console.log(address);
+  async function handleSubmit(e) {
+    e.preventDefault();
     //code here
+
   }
+
   return (
-    <>
-      <NavigationBar />
-      <form
-        style={{
-          width: "22rem",
-          alignItems: "center",
-          justifyContent: "center",
-          margin: "auto",
-          marginBottom: "100px",
-          paddingTop: "150px",
-        }}
-      >
-        <div class="card">
-          <div class="block">
-            <h1 class="heading text-center mb-4">
-              <b>SignUp</b>
-            </h1>
-          </div>
+    <Fragment>
+    <NavigationBar />
+      <form className="login_form" onSubmit={handleSubmit}>
+        <h1 className="login_heading">Signup</h1>
 
-          <MDBInput
-            className="mb-4"
+        <div className="input_element">
+        <MdEmail/>
+          <input
             type="email"
-            id="form2Example1"
-            label="Email"
+            value={email}
             onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            autoComplete="on"
           />
-          <MDBInput
-            className="mb-4"
-            type="text"
-            id="form2Example1"
-            label="Contact Number"
-            onChange={(e) => setContactNumber(e.target.value)}
+        </div>
+
+        <div className="input_element">
+        <RiLockPasswordFill/>
+          <input
+            type="number"
+            value={contactNo}
+            onChange={(e) => setContactNo(e.target.value)}
+            placeholder="Contact Number"
           />
-          <MDBInput
-            className="mb-4"
+        </div>
+
+        <div className="input_element">
+        <RiLockPasswordFill/>
+          <input
             type="text"
-            id="form2Example1"
-            label="Address"
+            value={address}
             onChange={(e) => setAddress(e.target.value)}
+            placeholder="Address"
           />
-          <MDBInput
-            className="mb-4"
+        </div>
+
+        <div className="input_element">
+        <RiLockPasswordFill/>
+          <input
             type="password"
-            id="form2Example2"
-            label="Password"
+            value={password}
             onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
           />
-          <MDBInput
-            className="mb-4"
+        </div>
+
+        <div className="input_element">
+        <RiLockPasswordFill/>
+          <input
             type="password"
-            id="form2Example2"
-            label="Confirm Password"
+            value={cpassword}
             onChange={(e) => setCpassword(e.target.value)}
+            placeholder="Confirm Password"
           />
-          <div class="rem">
-            <MDBRow className="mb-4">
-              <MDBCol className="d-flex justify-content-center">
-                <MDBCheckbox
-                  id="form2Example3"
-                  label="Remember me"
-                  defaultChecked
-                />
-              </MDBCol>
-            </MDBRow>
-          </div>
-          <MDBBtn
-            type="submit"
-            className="mb-4"
-            block
-            color="white"
-            onClick={handleSubmit}
-          >
-            Sign Up
-          </MDBBtn>
         </div>
-        <Link to="/login" style={{ display: "flex", justifyContent: "center" }}>
-          Already Registered ?
+
+        <button type="submit">Login</button>
+        <Link to="/signup" className="registerlink">
+          Create a new account?
         </Link>
-
-        <div className="text-center">
-          <div className="row my-4 d-flex justify-content-center">
-            <div>
-              <MDBBtn
-                type="button"
-                color="white"
-                rounded
-                className="mr-md-4 z-depth-1a"
-              >
-                <MDBIcon fab icon="facebook-f" className="blue-text " />
-              </MDBBtn>
-
-              <MDBBtn
-                type="button"
-                color="#000278 #192294 #5254D8 #B7B9F4"
-                rounded
-                className="mr-md-4 z-depth-1a"
-              >
-                <MDBIcon fab icon="google-plus-g" className="blue-text" />
-              </MDBBtn>
-            </div>
-          </div>
-        </div>
       </form>
       <Footer />
-    </>
+    </Fragment>
   );
 }
 
