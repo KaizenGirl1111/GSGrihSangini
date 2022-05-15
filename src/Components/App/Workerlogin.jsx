95bce6f07d7edb6b09ef4d61d256ddf5609e3b6e
Siddhesh Jain <jainsiddhesh777@gmail.com>
2022-05-15 18:32:46 +0530

    UI improved for worker login page

--- src/Components/App/Workerlogin.jsx
+++ src/Components/App/Workerlogin.jsx
@@ -1,78 +1,56 @@
-/* eslint-disable no-unused-vars */
-import React from "react";
-import "./workerlogin.css";
+import React, { Fragment, useState } from "react";
+import "./login.css";
+import { Link } from "react-router-dom";
 import NavigationBar from "./NavigationBar/NavigationBar";
 import Footer from "./Footer/Footer";
-import {
-  MDBInput,
-  MDBCol,
-  MDBRow,
-  MDBCheckbox,
-  MDBBtn,
-} from "mdb-react-ui-kit";
-import { Link } from "react-router-dom";
-function Workerlogin() {
-  const [visible, setVisible] = React.useState(false);
-  const [contactNumber,setContactNumber] = React.useState();
-  const [password,setPassword] = React.useState("");
-  return (
-    <>
-      <NavigationBar />
-      <form
-        style={{
-          width: "22rem",
-          alignItems: "center",
-          justifyContent: "center",
-          margin: "auto",
-          paddingTop: "100px",
-        }}
-      >
-        <div class="card">
-          <div class="block">
-            <h1 class="heading text-center mb-4">
-              <b>Login-Worker</b>
-            </h1>
-          </div>
-          <div className="abc"></div>
-          <MDBInput className="mb-4" label="MobileNo" />
-          <MDBInput
-            className="mb-4"
-            type={visible ? "text" : "password"}
-            id="form2Example2"
-            label="Password"
-            onChange={e => setContactNumber(e.target.value)}
-          />
-          <div class="rem">
-            <MDBRow className="mb-4">
-              <MDBCol className="d-flex justify-content-center">
-                <MDBCheckbox
-                  id="form2Example3"
-                  label="Show password"
-                  onChange={() => setVisible(!visible)}
-                />
-                <MDBCheckbox
-                  id="form2Example3"
-                  label="Remember me"
-                  defaultChecked
-                />
-              </MDBCol>
-              <Link
-                to="/Workersignup"
-                style={{ display: "flex", justifyContent: "center" }}
-              >
-                New worker?
-              </Link>
-            </MDBRow>
-          </div>
-          <MDBBtn type="submit" className="mb-4" block color="white">
-            Login
-          </MDBBtn>
-        </div>
-      </form>
+import {BsFillTelephoneFill} from "react-icons/bs";
+import {RiLockPasswordFill} from "react-icons/ri"
 
-      <Footer />
-    </>
+function Login() {
+  const [contactNo, setContactNo] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    //code here
+
+  }
+
+  return (
+    <Fragment>
+      <NavigationBar />
+      <form className="login_form" onSubmit={handleSubmit}>
+        <h1 className="login_heading">Worker Login</h1>
+
+        <div className="input_element">
+        <BsFillTelephoneFill/>
+          <input
+            type="number"
+            value={contactNo}
+            onChange={(e) => setContactNo(e.target.value)}
+            placeholder="Contact Number"
+            autoComplete="on"
+          />
+        </div>
+
+        <div className="input_element">
+        <RiLockPasswordFill/>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+        </div>
+
+        <button type="submit">Login</button>
+        <Link to="/Workersignup" className="registerlink">
+          Create a new account?
+        </Link>
+      </form>
+      <Footer/>
+    </Fragment>
   );
 }
 
-export default Workerlogin;
+export default Login;
