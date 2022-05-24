63eede59668e66290543512ff5a0c506ea1729ec
piyusha2001 <ppiyusha2001@gmail.com>
2022-05-25 00:00:16 +0530

    enhanced signin/signup UI

--- src/Components/App/Workerlogin.jsx
+++ src/Components/App/Workerlogin.jsx
@@ -1,128 +1,110 @@
-import React, { Fragment, useState } from "react";
-import "./login.css";
-import { Link ,useNavigate} from "react-router-dom";
-import NavigationBar from "./NavigationBar/NavigationBar";
-import Footer from "./Footer/Footer";
-
-import {BsFillTelephoneFill} from "react-icons/bs";
-import {RiLockPasswordFill} from "react-icons/ri";
-import axios from "axios";
-
-
-function Login() {
-  const[data,setData]=useState({
-    
-    mobileNo:"",
-    password:"",
-   
-
-  })
-
-  const navigate=useNavigate();
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-
-    await axios.post("http://localhost:5000/workerLogin",data)
-    navigate("./clientrequest");
-
-    const response = await fetch('http://localhost:5000/workerLogin', {
-      method: 'POST',
-
+import axios from 'axios';
+import React, { Fragment, useState } from 'react';
 import {
-  BsFillTelephoneFill,
-  BsFillEyeFill,
-  BsFillEyeSlashFill,
-} from "react-icons/bs";
-import { RiLockPasswordFill } from "react-icons/ri";
-
+	BsFillEyeFill,
+	BsFillEyeSlashFill,
+	BsFillTelephoneFill,
+} from 'react-icons/bs';
+import { RiLockPasswordFill } from 'react-icons/ri';
+import { Link, useNavigate } from 'react-router-dom';
+import Footer from './Footer/Footer';
+import './login.css';
+import NavigationBar from './NavigationBar/NavigationBar';
 
 function Login() {
-  const [contactNo, setContactNo] = useState("");
-  const [password, setPassword] = useState("");
-  const [visible, setVisible] = useState(false);
+	const [data, setData] = useState({
+		mobileNo: '',
+		password: '',
+	});
 
-  async function handleSubmit(e) {
-    e.preventDefault();
-    const response = await fetch("http://localhost:5000/workerLogin", {
-      method: "POST",
+	const navigate = useNavigate();
 
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ mobileNo: contactNo, password: password }),
-    });
-    const json = await response.json();
+	async function handleSubmit(e) {
+		e.preventDefault();
 
-    console.log(json)
+		await axios.post('http://localhost:5000/workerLogin', data);
+		navigate('./clientrequest');
 
+		const response = await fetch('http://localhost:5000/workerLogin', {
+			method: 'POST',
+		});
+	}
 
+	const [contactNo, setContactNo] = useState('');
+	const [password, setPassword] = useState('');
+	const [visible, setVisible] = useState(false);
 
-    console.log(json);
+	async function handleSubmit(e) {
+		e.preventDefault();
+		const response = await fetch('http://localhost:5000/workerLogin', {
+			method: 'POST',
 
-  }
-  function handleChange({currentTarget:input}){
-    setData({...data,[input.name]:input.value});
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ mobileNo: contactNo, password: password }),
+		});
+		const json = await response.json();
+
+		console.log(json);
+
+		console.log(json);
+	}
+	function handleChange({ currentTarget: input }) {
+		setData({ ...data, [input.name]: input.value });
+	}
+
+	return (
+		<Fragment>
+			<NavigationBar />
+			<div class='one'></div>
+			<form className='login_form' onSubmit={handleSubmit}>
+				<h1 className='login_heading'>Worker Login</h1>
+
+				<div className='input_element'>
+					<BsFillTelephoneFill />
+					<input
+						type='number'
+						name='mobileNo'
+						value={data.mobileNo}
+						onChange={handleChange}
+						placeholder='Contact Number'
+						autoComplete='on'
+					/>
+				</div>
+
+				<div className='input_element'>
+					<RiLockPasswordFill />
+					<input
+						name='password'
+						value={data.password}
+						onChange={handleChange}
+						type={visible ? 'text' : 'password'}
+						// onChange={(e) => setPassword(e.target.value)}
+
+						placeholder='Password'
+					/>
+					<div
+						onClick={() => {
+							setVisible(!visible);
+						}}
+					>
+						{visible ? (
+							<BsFillEyeFill fontSize='20px' />
+						) : (
+							<BsFillEyeSlashFill fontSize='20px' />
+						)}
+					</div>
+				</div>
+
+				<button type='submit'>Login</button>
+				<Link to='/Workersignup' className='registerlink'>
+					Create a new account?
+				</Link>
+			</form>
+			<Footer />
+		</Fragment>
+	);
 }
 
-  return (
-    <Fragment>
-      <NavigationBar />
-      <div class="one"></div>
-      <form className="login_form" onSubmit={handleSubmit}>
-        <h1 className="login_heading">Worker Login</h1>
-
-        <div className="input_element">
-          <BsFillTelephoneFill />
-          <input
-            type="number"
-            name="mobileNo"
-            value={data.mobileNo}
-            onChange={handleChange}
-            placeholder="Contact Number"
-            autoComplete="on"
-          />
-        </div>
-
-        <div className="input_element">
-          <RiLockPasswordFill />
-          <input
-
-            name="password"
-            type="password"
-            value={data.password}
-            onChange={handleChange}
-
-            type={visible ? "text" : "password"}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-
-            placeholder="Password"
-          />
-          <div onClick={()=>{setVisible(!visible)}}>
-          {visible ? <BsFillEyeFill fontSize="20px" /> : <BsFillEyeSlashFill fontSize="20px"/>}
-          </div>
-        </div>
-
-        <button type="submit">Login</button>
-        <Link to="/Workersignup" className="registerlink">
-          Create a new account?
-        </Link>
-      </form>
-      <Footer />
-    </Fragment>
-  );
-}
-
-
-
 export default Login;
-
-
-
-
-export default Login;
-
-
-
-
